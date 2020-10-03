44a0c3308c35fd1957a3324e05209a1ca6e84c74
Hai Liang Wang <hain@chatopera.com>
2020-10-03 09:56:44 +0800

    https://github.com/chatopera/clause/issues/23 release latest dist files

--- admin/release.sh
+++ admin/release.sh
@@ -1,7 +1,7 @@
 #! /bin/bash 
-###########################################
-# 
-###########################################
+#####################################################
+# Relase new packages to dist branch with osc files
+#####################################################
 
 # constants
 baseDir=$(cd `dirname "$0"`;pwd)
@@ -20,49 +20,83 @@ if [ ! -d $baseDir/../tmp ]; then
     mkdir $baseDir/../tmp
 fi
 
+## Check git branch and status
+GIT_BRANCH=`git branch | awk '/^\*/{print $2}'`
+
+if [[ ! $GIT_BRANCH -eq 'osc' ]]; then
+    echo "current branch is not osc."
+    echo "switch to osc branch at first and release again."
+    exit 1
+fi
+
+if [[ ! -z $(git status -s) ]]; then
+    git status
+    echo 'modified/untracked, fix them to keep clean and release again.'
+    exit 2
+fi
+
+## Check if release dir exists.
+relasedir=$baseDir/../../clause.dist
+if [ ! -d $relasedir ]; then
+    echo "ERROR" $relasedir "not exist"
+    echo "Use following scripts to setup release dir at first."
+    echo "  cd" `cd $baseDir/../..;pwd`
+    echo "  git clone https://github.com/chatopera/clause.git -b dist clause.dist"
+    exit 1
+fi
+relasedir=`cd $relasedir; pwd`
+
+## Create temp dir
 cd $baseDir/..
 GIT_COMMIT_SHORT=`git rev-parse --short HEAD`
-relasedir=$baseDir/../tmp/clause.dist.$GIT_COMMIT_SHORT
+tmpdir=$baseDir/../tmp/$GIT_COMMIT_SHORT
 
-cd $baseDir/../tmp
-rm -rf $relasedir; mkdir $relasedir
+if [ -d $tmpdir ]; then
+    rm -rf $tmpdir
+fi
+echo "generate dir for temporary files $tmpdir ..."
+mkdir -p $tmpdir
 
-# copy config file
-cp $rootDir/sample.env $relasedir/.env
-cp $rootDir/docker-compose.yml $relasedir
-cp $rootDir/LICENSE $relasedir
-cp $rootDir/README.md $relasedir
+# Copy release files
+## copy config
+cd $baseDir/..
+cp {sample.env,docker-compose.yml,LICENSE,README.md} $tmpdir
 
-# copy database persistence folders
-mkdir $relasedir/var
-cp -rf $rootDir/var/activemq $relasedir/var
-$SUDO_CMD rm -rf $relasedir/var/activemq/data/*
+## copy database persistence folders
+mkdir $tmpdir/var
+cp -rf var/activemq $tmpdir/var
+$SUDO_CMD rm -rf $tmpdir/var/activemq/data/*
 
-cp -rf $rootDir/var/redis $relasedir/var
-$SUDO_CMD rm -rf $relasedir/var/redis/data/*
+cp -rf var/redis $tmpdir/var
+$SUDO_CMD rm -rf $tmpdir/var/redis/data/*
 
-cp -rf $rootDir/var/mysql $relasedir/var
-$SUDO_CMD rm -rf $relasedir/var/mysql/data/*
+cp -rf var/mysql $tmpdir/var
+$SUDO_CMD rm -rf $tmpdir/var/mysql/data/*
 
-# copy clause data folder
-mkdir $relasedir/var/local
-cp -rf $rootDir/var/local/data $relasedir/var/local/data
-mkdir -p $relasedir/var/local/workarea
+## copy clause data folder
+mkdir $tmpdir/var/local
+cp -rf var/local/data $tmpdir/var/local/data
+mkdir -p $tmpdir/var/local/workarea
 
-# copy clause mysql database init script
-mkdir -p $relasedir/src/clause/config
-cp -rf $rootDir/src/clause/config/sql $relasedir/src/clause/config
+## copy clause mysql database init script
+mkdir -p $tmpdir/src/clause/config
+cp -rf src/clause/config/sql $tmpdir/src/clause/config
 
-# copy admin scripts
-mkdir $relasedir/admin
-cp -rf $rootDir/admin/docker $relasedir/admin
+## copy admin scripts
+mkdir $tmpdir/admin
+cp -rf admin/{flush.sh,start.sh,stop.sh} $tmpdir/admin
 
-# init git
-cp -rf $rootDir/.gitignore $relasedir
+## generate BUILD file to save commit id
+echo $GIT_COMMIT_SHORT > $tmpdir/BUILD
+echo "" >> $tmpdir/sample.env
+echo "CLAUSE_BUILD="$GIT_COMMIT_SHORT >> $tmpdir/sample.env
+
+# Override release dir
 cd $relasedir
-git init 
-git add --all
-git commit -m "init"
-cd ..
+rm -rf *
+cd $tmpdir
+tar cf - .|(cd $relasedir;tar xf -)
+cd $relasedir
+git status
 
-tar czf clause.dist.$GIT_COMMIT_SHORT.tgz clause.dist.$GIT_COMMIT_SHORT
+echo $relasedir "is re-generated with" $GIT_COMMIT_SHORT "files."
